0974b34e839c50fe6697a0df0169ffe27518cea4
Frank-Chandler <54063330+Frank-Chandler@users.noreply.github.com>
2022-07-03 17:01:20 +0800

    1

--- chapt1/1_02.cpp
+++ chapt1/1_02.cpp
@@ -7,11 +7,78 @@ int main()
     string user_name;
     int val = 0;
     int num_tries = 0;
+    int user_guess = 0;
     int num_right(0);
     double user_score=0.0;
     char user_more;
+    char user_rsp;
     bool go_for_it=true;
-    const int max_tries=3;
+    bool got_it=true;
+    bool next_seq=true;
 
+    const int max_tries=3;
+    const int seq_size=18;
+    const int max_seq=6;
+    int elem_seq[seq_size]={
+        1,2,3,
+        3,4,7,
+        2,5,12,
+        3,6,10,
+        4,9,16,
+        5,12,22
+    };
+    string seq_name[max_seq]=
+    {
+        "Fibonacci",
+        "Lucas",
+        "Pell",
+        "Triangular",
+        "Square",
+        "Pentagona"
+    };
+    int cur_index= 0;
+    while (next_seq == true && cur_index<seq_size)
+    {
+        cout<<"the first 2 elements of the sequence are:"<< elem_seq[cur_index]<<","<< elem_seq[cur_index+1]<<endl;
+        cout<<"what is hte next element? ";
+        int tries_cnt=0;
+        next_seq=true;
+        go_for_it=true;
+        got_it=false;
+        while (!got_it&&go_for_it&&(++tries_cnt<=max_tries))
+        {   if (user_guess==elem_seq[cur_index+2])
+        {
+            ++num_right;
+            got_it=true;
+        }else{
+            switch (tries_cnt)
+            {
+            case 1:
+                cout<<"Oops,Nice guess but not quite it."<<endl;
+                break;
+            case 2:
+                cout<<"Sorry.Wrong a second time."<<endl;
+                break;
+            case 3:
+                cout<<"Ah,this is  harder than it looks,isn't it?"<<endl;
+                break;
+        
+            default:
+                cout<<"It must be getting pretty frustrating by more tries."<<endl;
+                break;
+            }
+        }
+        cout<<"do you want to continue?(y/n):"<<endl;
+        cin >> user_rsp;
+        if (user_rsp=='y'||user_rsp=='Y')
+            {
+            go_for_it=true;
+            }else if (user_rsp=='n'||user_rsp=='N')
+                {
+            go_for_it=false;
+                }
+    
+        }
+    }
     return 0;
 }
--- chapt1/1_03.cpp
+++ chapt1/1_03.cpp
@@ -7,19 +7,20 @@ int main()
 {
     
     const int seq_size = 18;
-    int pell_seq[seq_size];
     vector <int> pell_Seq(seq_size);
-    pell_seq[0]=1;
-    pell_seq[1]=2;
-    for(int ix=2;ix<seq_size;++ix)
+    pell_Seq[0]=1;
+    pell_Seq[1]=2;
+    for (int index = 1; i < seq_size; index++)
     {
-        pell_seq[ix]=pell_seq[ix-2]+2*pell_Seq[ix-1];
+        pell_Seq[index]=2*pell_Seq[index-1]+pell_Seq[index-2];
     }
-    cout<<"the first "<<seq_size<<" elements of Pell Series:\n\t";
-    for(int ix=0;ix<seq_size;++ix){
-        cout<<pell_Seq[ix]<<' ';
-        cout<<'\n';
+    cout<<"the first" <<pell_Seq.size()<<"element of the pell series:";
+    for (int index = 0; index < seq_size; index++)
+    {
+        cout<<pell_Seq[index]<<" ";
     }
+    cout<<endl;
     
-
+    
+    return 0;
 }
