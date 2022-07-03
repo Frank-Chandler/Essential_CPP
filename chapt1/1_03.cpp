bef7f510ff4ce5510e46c369610acf716ec2a77b
Frank-Chandler <54063330+Frank-Chandler@users.noreply.github.com>
2022-07-03 15:44:05 +0800

    1

--- chapt1/1_03.cpp
+++ chapt1/1_03.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 int main()
 {
     
+    const int seq_size = 18;
+    int pell_seq[seq_size];
+    vector <int> pell_Seq(seq_size);
+    pell_seq[0]=1;
+    pell_seq[1]=2;
+    for(int ix=2;ix<seq_size;++ix)
+    {
+        pell_seq[ix]=pell_seq[ix-2]+2*pell_Seq[ix-1];
+    }
+    cout<<"the first "<<seq_size<<" elements of Pell Series:\n\t";
+    for(int ix=0;ix<seq_size;++ix){
+        cout<<pell_Seq[ix]<<' ';
+        cout<<'\n';
+    }
+    
+
 }
